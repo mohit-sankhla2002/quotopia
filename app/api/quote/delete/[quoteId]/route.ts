ed546b86dd860262211f187d4cbc5bbec11cb758
mohit-sankhla2002 <100336545+mohit-sankhla2002@users.noreply.github.com>
2023-10-24 19:19:38 +0530

    added the delete and edit routes for the quotes

---
+++ app/api/quote/delete/[quoteId]/route.ts
@@ -0,0 +1,52 @@
+import { NextRequest } from "next/server";
+import Quote from "@/models/quote";
+import { connectToDb } from "@/utils/db";
+import { z } from 'zod';
+
+const validator = z.object({
+    quote: z.string(), 
+    author: z.string(), 
+    tag: z.string()
+});
+
+export async function DELETE(req: NextRequest) {
+    try {
+        await connectToDb();
+        const quoteId: string = req.url.split("/")[req.url.split("/").length - 1];
+        await Quote.findByIdAndDelete(quoteId);
+        return new Response(JSON.stringify({
+            message: `Quote with id: ${quoteId} was delete successfully`
+        }), {
+            status: 204
+        })
+    } catch (error: any) {
+        console.log(error);
+        return new Response(JSON.stringify({
+            message: error.message
+        }), {
+            status: 500
+        })
+    }
+};
+
+export async function PATCH(req: NextRequest) {
+    try {
+        await connectToDb();
+        const body = await req.json();
+        const parsedBody = validator.parse(body);
+        const quoteId: string = req.url.split("/")[req.url.split("/").length - 1];
+        const quote = await Quote.findByIdAndUpdate(quoteId, parsedBody);
+
+        return new Response(JSON.stringify(quote), {
+            status: 201
+        })
+
+    } catch (error: any) {
+        console.log(error);
+        return new Response(JSON.stringify({
+            message: error.message
+        }), {
+            status: 201
+        });
+    }
+}
