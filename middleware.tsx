fb01eaa683e3bc37b75cda0e062c9dfe8ca6391a
mohit-sankhla2002 <100336545+mohit-sankhla2002@users.noreply.github.com>
2023-10-24 19:50:13 +0530

    secured frontend routes

--- middleware.tsx
+++ middleware.tsx
@@ -4,12 +4,12 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
     const userToken = request.cookies.get('next-auth.session-token')?.value;
     if (!userToken) {
-        return NextResponse.redirect(new URL('/desired-route', request.url))
+        return NextResponse.redirect(new URL('/unauthenticated', request.url))
     }
     else {
-        return NextResponse.redirect(new URL('/desired-route', request.url))
+        return NextResponse.redirect(new URL(request.url))
     }
 }
 export const config = {
-    matcher: '/desired-route',
+    matcher: ['/profile', '/edit-quote'],
 }
